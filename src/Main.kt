b5505b2aa3063ee5a7d1118ff052e23efa6f0882
eddieb <461eb19@students.olatheschools.com>
2018-10-20 12:47:26 -0500

    10-20-2018 12:47 PM CST: Lib Update

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,7 @@
 val title = "The Quest Of The Breadstick"
 fun main(args: Array<String>) {
     println("Welcome to the "+title)
+    println("Enter the name of the hero:")
+    val heroname = readLine().toString()
+    println(heroname)
 }
