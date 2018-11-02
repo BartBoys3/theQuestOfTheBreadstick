1c5976106a721eafa9a0699c370f42f70805968a
Edward "BartBoys3" Bartelli <461eb19@students.olatheschools.com>
2018-11-01 22:16:28 -0500

    11-1-2018 10:16 PM CDT

---
+++ src/com/BartBoys3/theQuestOfTheBreadstick/testing.kt
@@ -0,0 +1,11 @@
+package com.BartBoys3.theQuestOfTheBreadstick
+
+import java.util.*
+    val x = 0
+    fun main(args: Array<String>) {
+     class Test {
+         var newTextVarSnippet = TextVars
+
+     }
+    }
+ 
