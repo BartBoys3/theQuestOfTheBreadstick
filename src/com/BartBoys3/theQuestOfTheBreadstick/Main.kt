af078e0b6e66307fb58ef95f2d1def56aa686c15
Edward "BartBoys3" Bartelli <461eb19@students.olatheschools.com>
2018-11-18 20:56:02 -0600

    added globalFuns.StartMidi 11/18/2018

--- src/com/BartBoys3/theQuestOfTheBreadstick/Main.kt
+++ src/com/BartBoys3/theQuestOfTheBreadstick/Main.kt
@@ -18,16 +18,17 @@ import kotlin.math.absoluteValue
     val sequencer = MidiSystem.getSequencer()
     fun main(args: Array<String>) {
         println("Welcome to " + title)
-        sequencer.open()
-        inputStream = BufferedInputStream(FileInputStream(File("c:/users/bartboys3/music/tQotB-TitleTheme-11.18.2018.mid")))
-        sequencer.setSequence(inputStream)
-        sequencer.start()
+        globalFuns.StartMidi("U6-BOOT.midi")
+        println("Press enter to contiune")
+        val x = readLine().toString()
         println("do not enter anything stupid into a prompt or else the game is very likely to terminate.".toUpperCase())
         Heraname.startgame()
     }
 
     fun optmenu(opts: Array<String>, questnum: Int) {
-        if (questnum >= 0) {println("The Day is " + day)}
+        if (questnum >= 0) {
+            println("The Day is " + day)
+    }
         println(opttext)
         println("1. ${opts[0]}")
         println("2. ${opts[1]}")
@@ -35,11 +36,15 @@ import kotlin.math.absoluteValue
         println("4. ${opts[3]}")
         println("5. ${opts[4]}")
         println("Number Here")
-        if (questnum >= 0) {optdb.add(questnum, readLine().toString().toInt())}
-        else {negativeQuestTempStorage = readLine().toString().toInt()}
+        if (questnum >= 0) {
+            optdb.add(questnum, readLine().toString().toInt())
+        } else {
+            negativeQuestTempStorage = readLine().toString().toInt()
+        }
         select(questnum)
     }
-            //Rolls over from 255 to 0 and from 0 to 255 :D
+
+    //Rolls over from 255 to 0 and from 0 to 255 :D
     fun addDay(n: Int) {
         if (day + n > 255) {
             day = ((255 - day) - 1).toShort()
@@ -51,18 +56,16 @@ import kotlin.math.absoluteValue
             }
         }
     }
-
- object Heraname {
-     @JvmStatic
-     public fun startgame() {
-         println("Enter the name of the hero:")
-         val heroname = readLine().toString()
-         println("Your journey starts in the small village of Hickory Peaks.")
-         println("You gotta defeat some evil dude to get back your breadstick.")
-         println()
-         Gear.Inv.itemGet("Stick", 20)
-         Gear.Inv.itemGet("Sticki", 20)
-         askQuest(0)
-         var z = heroname.toCharArray()
-     }
- }
+object Heraname {
+    fun startgame() {
+        println("Enter the name of the hero:")
+        val heroname = readLine().toString()
+        println("Your journey starts in the small village of Hickory Peaks.")
+        println("You gotta defeat some evil dude to get back your breadstick.")
+        println()
+        Gear.Inv.itemGet("Stick", 20)
+        Gear.Inv.itemGet("Sticki", 20)
+        askQuest(0)
+        var z = heroname.toCharArray()
+    }
+}
--- src/com/BartBoys3/theQuestOfTheBreadstick/globalFuns.kt
+++ src/com/BartBoys3/theQuestOfTheBreadstick/globalFuns.kt
@@ -1,4 +1,15 @@
 package com.BartBoys3.theQuestOfTheBreadstick
 
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+
 object globalFuns {
+    fun StartMidi(FileMusicDir: String) {
+        sequencer.open()
+        inputStream = BufferedInputStream(FileInputStream(File("c:/users/bartboys3/music/${FileMusicDir}")))
+        sequencer.setSequence(inputStream)
+        sequencer.start()
+    }
 }
